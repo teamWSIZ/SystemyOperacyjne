b0dc28bc512973ad1e821a864152622801a59cf0
teamWSIZ <teamWSIZ@users.noreply.github.com>
2015-01-30 16:53:25 +0100

    Update syntaksBash_a.sh

--- syntaksBash_a.sh
+++ syntaksBash_a.sh
@@ -7,3 +7,17 @@ echo $mojaZmienna
 echo $innaZmienna
 
 cat /etc/passwd | grep $innaZmienna
+
+# to jest komentarz
+echo Pierwszy argument to: $1     #pierwszy argument skryptu
+echo Drugi argument to: $2     #drugi argument skryptu
+echo Liczba argumentów: $#
+echo Wszystkie argumenty $@
+
+## Zmienne, wykonywanie komend linuxa, sprawdzanie czy dały wynik (jakiś) czy pusty
+user="tttuuu"
+wynik=`cat /etc/passwd | grep $user`
+
+if [[ -z "${wynik}" ]]; then
+  echo "User $user nie istnieje na tym systemie"
+fi
